eb2f355f72749665d3031d95548e362c4a3f8cea
Anthony Hevia <anthonyhevia@microsoft.com>
2022-09-19 11:33:36 -0700

    Start api & db dev

---
+++ app/api/main.py
@@ -0,0 +1,24 @@
+from typing import Optional
+from fastapi import Body, Request, FastAPI, HTTPException
+from pydantic import BaseModel
+from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# add our app middleware
+origins = [
+    "http://localhost:3000",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3232",
+    "http://localhost:5000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
